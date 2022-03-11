6fe36596b89542da44bec1dc0bf8575349f9c493
James <gekylin@gmail.com>
2022-03-11 17:54:29 +0800

    feat: 完成全局异常

--- venus-starter/venus-starter-core/src/main/java/com/junmoyu/venus/cloud/starter/core/exception/ExceptionHandlers.java
+++ venus-starter/venus-starter-core/src/main/java/com/junmoyu/venus/cloud/starter/core/exception/ExceptionHandlers.java
@@ -1,7 +1,6 @@
 package com.junmoyu.venus.cloud.starter.core.exception;
 
 import com.junmoyu.venus.cloud.starter.core.model.dto.Response;
-import com.junmoyu.venus.cloud.starter.core.model.dto.ResponseCode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -10,7 +9,6 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.yaml.snakeyaml.constructor.DuplicateKeyException;
 
 import java.util.Objects;
 import java.util.Optional;
@@ -39,70 +37,69 @@ public class ExceptionHandlers {
         }
         return Response.failure(message);
     }
-//
+
 //    @ExceptionHandler(DuplicateKeyException.class)
-//    protected ShenyuAdminResult handleDuplicateKeyException(final DuplicateKeyException exception) {
+//    protected Response<String> handleDuplicateKeyException(final DuplicateKeyException exception) {
 //        log.error("duplicate key exception ", exception);
-//        return ShenyuAdminResult.error(ShenyuResultMessage.UNIQUE_INDEX_CONFLICT_ERROR);
+//        return Response.failure("unique index conflict, please enter again");
 //    }
-//
+
 //    @ExceptionHandler(UnauthorizedException.class)
-//    protected ShenyuAdminResult handleUnauthorizedException(final UnauthorizedException exception) {
+//    protected Response<String> handleUnauthorizedException(final UnauthorizedException exception) {
 //        log.error("unauthorized exception", exception);
-//        return ShenyuAdminResult.error(CommonErrorCode.TOKEN_NO_PERMISSION, ShenyuResultMessage.TOKEN_HAS_NO_PERMISSION);
+//        return Response.failure(CommonErrorCode.TOKEN_NO_PERMISSION, ShenyuResultMessage.TOKEN_HAS_NO_PERMISSION);
 //    }
-//
-//    @ExceptionHandler(NullPointerException.class)
-//    protected ShenyuAdminResult handleNullPointException(final NullPointerException exception) {
-//        log.error("null pointer exception ", exception);
-//        return ShenyuAdminResult.error(CommonErrorCode.NOT_FOUND_EXCEPTION, ShenyuResultMessage.NOT_FOUND_EXCEPTION);
-//    }
-//
-//    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-//    protected ShenyuAdminResult handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException e) {
-//        LOG.warn("http request method not supported", e);
-//        StringBuilder sb = new StringBuilder();
-//        sb.append(e.getMethod());
-//        sb.append(
-//                " method is not supported for this request. Supported methods are ");
-//        Objects.requireNonNull(e.getSupportedHttpMethods()).forEach(t -> sb.append(t).append(" "));
-//        return ShenyuAdminResult.error(sb.toString());
-//    }
-//
-//    @ExceptionHandler(MethodArgumentNotValidException.class)
-//    protected ShenyuAdminResult handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
-//        LOG.warn("method argument not valid", e);
-//        BindingResult bindingResult = e.getBindingResult();
-//        String errorMsg = bindingResult.getFieldErrors().stream()
-//                .map(f -> f.getField().concat(": ").concat(Optional.ofNullable(f.getDefaultMessage()).orElse("")))
-//                .collect(Collectors.joining("| "));
-//        return ShenyuAdminResult.error(String.format("Request error! invalid argument [%s]", errorMsg));
-//    }
-//
-//    @ExceptionHandler(MissingServletRequestParameterException.class)
-//    protected ShenyuAdminResult handleMissingServletRequestParameterException(final MissingServletRequestParameterException e) {
-//        LOG.warn("missing servlet request parameter", e);
-//        return ShenyuAdminResult.error(String.format("%s parameter is missing", e.getParameterName()));
-//    }
-//
-//    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-//    protected ShenyuAdminResult handleMethodArgumentTypeMismatchException(final MethodArgumentTypeMismatchException e) {
-//        LOG.warn("method argument type mismatch", e);
-//        return ShenyuAdminResult.error(String.format("%s should be of type %s", e.getName(), Objects.requireNonNull(e.getRequiredType()).getName()));
-//    }
-//
+
+    @ExceptionHandler(NullPointerException.class)
+    protected Response<String> handleNullPointException(final NullPointerException exception) {
+        log.error("null pointer exception ", exception);
+        return Response.failure("not found exception");
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected Response<String> handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException e) {
+        log.warn("http request method not supported", e);
+        StringBuilder sb = new StringBuilder();
+        sb.append(e.getMethod());
+        sb.append(" method is not supported for this request. Supported methods are ");
+        Objects.requireNonNull(e.getSupportedHttpMethods()).forEach(t -> sb.append(t).append(" "));
+        return Response.failure(sb.toString());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected Response<String> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
+        log.warn("method argument not valid", e);
+        BindingResult bindingResult = e.getBindingResult();
+        String errorMsg = bindingResult.getFieldErrors().stream()
+                .map(f -> f.getField().concat(": ").concat(Optional.ofNullable(f.getDefaultMessage()).orElse("")))
+                .collect(Collectors.joining("| "));
+        return Response.failure(String.format("Request error! invalid argument [%s]", errorMsg));
+    }
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    protected Response<String> handleMissingServletRequestParameterException(final MissingServletRequestParameterException e) {
+        log.warn("missing servlet request parameter", e);
+        return Response.failure(String.format("%s parameter is missing", e.getParameterName()));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected Response<String> handleMethodArgumentTypeMismatchException(final MethodArgumentTypeMismatchException e) {
+        log.warn("method argument type mismatch", e);
+        return Response.failure(String.format("%s should be of type %s", e.getName(), Objects.requireNonNull(e.getRequiredType()).getName()));
+    }
+
 //    @ExceptionHandler(ConstraintViolationException.class)
-//    protected ShenyuAdminResult handleConstraintViolationException(final ConstraintViolationException e) {
-//        LOG.warn("constraint violation exception", e);
+//    protected Response<String> handleConstraintViolationException(final ConstraintViolationException e) {
+//        log.warn("constraint violation exception", e);
 //        Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
-//        return ShenyuAdminResult.error(violations.stream()
+//        return Response.failure(violations.stream()
 //                .map(v -> v.getPropertyPath().toString().concat(": ").concat(v.getMessage()))
 //                .collect(Collectors.joining("| ")));
 //    }
-//
-//    @ExceptionHandler(ShenyuAdminException.class)
-//    protected ShenyuAdminResult handleVenusException(final ShenyuAdminException exception) {
-//        log.error("null pointer exception ", exception);
-//        return ShenyuAdminResult.error(CommonErrorCode.NOT_FOUND_EXCEPTION, exception.getMessage());
-//    }
+
+    @ExceptionHandler(VenusException.class)
+    protected Response<String> handleVenusException(final VenusException exception) {
+        log.error("venus exception ", exception);
+        return Response.failure(exception.getCode(), exception.getMessage());
+    }
 }
